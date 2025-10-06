aa020282b4622bdceff4043c222cb19e88a31730
jiswan <muhammedjiswan@gmail.com>
2025-10-06 00:17:22 -0400

    Collection

---
+++ Control Flow Statements/IF Statement/SimpleIF/SimpleIf.java
@@ -0,0 +1,13 @@
+public class SimpleIf {
+    public static void main(String[] args) {
+        int a= 18;
+        if (a>=18) {
+            System.out.println("Hello number is Equal");
+            
+        } else {
+
+            System.out.println("Hello number is not Equal");
+        }
+    }
+
+}

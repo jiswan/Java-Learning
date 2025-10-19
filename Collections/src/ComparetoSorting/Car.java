f1798f06eb31e09dfc0f19ba3135bb91b6817f42
jiswan <muhammedjiswan@gmail.com>
2025-10-18 21:07:11 -0400

    Compare To Collection

---
+++ Collections/src/ComparetoSorting/Car.java
@@ -0,0 +1,16 @@
+package ComparetoSorting;
+
+public class Car {
+
+    public String carName;
+    public String engineType;
+
+    public Car(String carName, String engineType) {             // constructor 
+        this.carName = carName;
+        this.engineType = engineType;
+    }
+
+    
+
+
+}
